4781fb89855eac9149b151902a13d82777366b7c
leanne404 <90459334+leanne404@users.noreply.github.com>
2023-01-05 19:52:51 +0800

    server ip

--- frontend/src/containers/hooks/useChess.js
+++ frontend/src/containers/hooks/useChess.js
@@ -2,8 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react"
 import { message } from 'antd'
 
 // const SERVER_IP = '192.168.0.144'
-const SERVER_IP = 'localhost'
-const clientWS = new WebSocket( 'ws://' + SERVER_IP + ':4000' )
+const clientWS = new WebSocket( "https://wpchess-production-71c8.up.railway.app" )
 
 const sendData = async ( data ) => {
     await clientWS.send( JSON.stringify( data ) )
